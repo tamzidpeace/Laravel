dde17b1fe5c30cc7bea5035e50f9cd5d62a2e5fc
tamzidpeace <tamjedpeace@gmail.com>
2019-05-01 13:02:20 +0600

    raw sql crud

--- cms/app/Http/routes.php
+++ cms/app/Http/routes.php
@@ -18,12 +18,12 @@ Route::get('/', function () {
 // route with parameter
 
 Route::get('/post/{id}/{title}', function ($id, $title) {
-    return $id . " " . $title; 
+    return $id . " " . $title;
 });
 
 //naming routes
 
-Route::get('admin/posts/example', array ( 'as' => 'admin.home' ,function () {
+Route::get('admin/posts/example', array('as' => 'admin.home', function () {
     return "123";
 }));
 
@@ -40,3 +40,36 @@ Route::get('/contact', 'PostController@contact');
 
 // passing data to view route
 Route::get('/mPost/{id}/{name}', 'PostController@post');
+
+//inserting using raw sql queries
+Route::get('/insert', function () { 
+    DB::insert('insert into posts(title,body) values(?,?)',['test0','test number 0']);
+    return "inserted";
+});
+
+
+//read using raw sql queries
+Route::get('/read', function () { 
+    $result = DB::select('select * from posts');
+    
+    foreach($result as $post) {
+        return $post->title . " " . $post->body;
+    }
+
+    //return $result;
+    
+});
+
+//update using raw sql queries
+Route::get('/update', function () { 
+    
+    $updated = DB::update('update posts set title="new title" where id = ?', [1]);
+    return $updated;
+});
+
+//delete using raw sql queries
+Route::get('/delete', function () { 
+    
+    $deleted = DB::delete('delete from posts where id = ?', [1]);
+    return $deleted;
+});
