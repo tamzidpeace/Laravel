4adce7938de09aca2baa65d687f40dd5bf94e2b4
tamzidpeace <tamjedpeace@gmail.com>
2019-05-02 23:52:51 +0600

    one to one insert

--- onetoone/app/Http/routes.php
+++ onetoone/app/Http/routes.php
@@ -10,7 +10,19 @@
 | and give it the controller to call when that URI is requested.
 |
 */
+use App\User;
+use App\Address;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/insert/{id}', function($id) {
+    $user = User::findOrFail($id);
+    $address = new Address(['name'=>'cumilla']);
+
+    $user->address()->save($address);
+    return 'data inserted';
+});
+
+
--- onetoone/app/User.php
+++ onetoone/app/User.php
@@ -4,6 +4,7 @@ namespace App;
 
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
+
 class User extends Authenticatable
 {
     /**
@@ -23,4 +24,8 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    public function address() {
+        return $this->hasOne('App\Address');
+    }
 }
