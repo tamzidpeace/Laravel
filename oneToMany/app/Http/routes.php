bd6ebad06a55be14a48ab2a274974202ae9d74cb
tamzidpeace <tamjedpeace@gmail.com>
2019-05-03 11:18:45 +0600

    one to many --insert

--- oneToMany/app/Http/routes.php
+++ oneToMany/app/Http/routes.php
@@ -10,7 +10,18 @@
 | and give it the controller to call when that URI is requested.
 |
 */
+use App\User;
+use App\Post;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+//insert
+Route::get('/insert', function() {
+    $user = User::findOrFail(2);
+    $post = new Post(['title'=>'test', 'content'=>'test 2']);
+    $user->post()->save($post);
+
+    return 'inserted';
 });
