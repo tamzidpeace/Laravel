f1bc9bdc2aac06cf07305249d5e8171c0b717373
tamzidpeace <tamjedpeace@gmail.com>
2019-05-03 18:05:56 +0600

    many to many all crud

--- manyToMany/app/Http/routes.php
+++ manyToMany/app/Http/routes.php
@@ -11,6 +11,53 @@
 |
 */
 
+use App\User;
+use App\Role;
+
 Route::get('/', function () {
     return view('welcome');
+});
+
+//insert
+Route::get('/insert', function () {
+    $user = User::findOrFail(1);
+    $role = new Role(['name' => 'subscriber']);
+    $user->roles()->save($role);
+
+    return "data inserted";
+});
+
+
+//read
+Route::get('/read', function () {
+    $user = User::findOrFail(1);
+
+    foreach ($user->roles as $role) {
+        echo $role->name;
+    }
+});
+
+//update
+Route::get('/update', function () {
+    $user = User::findOrFail(1);
+    //echo $user;
+
+
+    // something is worng with it
+    if ($user->has('role')) {
+        foreach ($user->roles as $role) {
+            if ($role->name == 'jkadjf') {
+                $role->name = 'Admin';
+                $role->save();
+            }
+        }
+    }
+});
+
+//delete
+Route::get('/delete', function () {
+    $user = User::findOrFail(1);
+
+    $user->roles()->delete();
+    
 });
